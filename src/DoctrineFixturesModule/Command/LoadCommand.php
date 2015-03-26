0c29e56db787ad6af0252c2825957891b2a7bee4
Kenneth Kataiwa <kenkataiwa@gmail.com>
2015-03-26 17:44:57 +0300

    Improve --help, add asking before execution

--- src/DoctrineFixturesModule/Command/LoadCommand.php
+++ src/DoctrineFixturesModule/Command/LoadCommand.php
@@ -25,7 +25,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Doctrine\ORM\Tools\SchemaTool;
-use Doctrine\DBAL\Migrations\Configuration\Configuration;
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use DoctrineFixturesModule\Loader\ServiceLocatorAwareLoader;
@@ -45,15 +44,15 @@ class LoadCommand extends Command
     protected $paths;
 
     protected $em;
-    
+
     /**
      * Service Locator instance
-     * @var Zend\ServiceManager\ServiceLocatorInterface
+     * @var \Zend\ServiceManager\ServiceLocatorInterface
      */
     protected $serviceLocator;
 
     const PURGE_MODE_TRUNCATE = 2;
-    
+
     public function __construct(ServiceLocatorInterface $serviceLocator)
     {
         $this->serviceLocator = $serviceLocator;
@@ -62,39 +61,75 @@ class LoadCommand extends Command
 
     protected function configure()
     {
-        parent::configure();
+        $this->setName('fixtures:load')
+            ->setDescription('Load data fixtures to your database.')
+            ->addOption('fixtures', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'The directory or file to load data fixtures from.')
+            ->addOption('append', null, InputOption::VALUE_NONE, 'Append the data fixtures instead of deleting all data from the database first.')
+            ->addOption('em', null, InputOption::VALUE_REQUIRED, 'The entity manager to use for this command.')
+            ->addOption('purge-with-truncate', null, InputOption::VALUE_NONE, 'Purge data by using a database-level TRUNCATE statement')
+            ->setHelp(<<<EOT
+The <info>fixtures:load</info> command loads data fixtures from your bundles:
 
-        $this->setName('data-fixture:import')
-            ->setDescription('Import Data Fixtures')
-            ->setHelp(
-<<<EOT
-The import command Imports data-fixtures
+  <info>fixtures:load</info>
+
+You can also optionally specify the path to fixtures with the <info>--fixtures</info> option:
+
+  <info>fixtures:load --fixtures=/path/to/fixtures1 --fixtures=/path/to/fixtures2</info>
+
+If you want to append the fixtures instead of flushing the database first you can use the <info>--append</info> option:
+
+  <info>fixtures:load --append</info>
+
+By default Doctrine Data Fixtures uses DELETE statements to drop the existing rows from
+the database. If you want to use a TRUNCATE statement instead you can use the <info>--purge-with-truncate</info> flag:
+
+  <info>fixtures:load --purge-with-truncate</info>
 EOT
-            )
-            ->addOption('append', null, InputOption::VALUE_NONE, 'Append data to existing data.')
-            ->addOption('purge-with-truncate', null, InputOption::VALUE_NONE, 'Truncate tables before inserting data');
+            );
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $em = $this->em;
+
+        if ($input->isInteractive() && !$input->getOption('append')) {
+            $dialog = $this->getHelperSet()->get('dialog');
+            if (!$dialog->askConfirmation($output, '<question>Careful, database will be purged. Do you want to continue Y/N ?</question>', false)) {
+                return;
+            }
+        }
+
+        $dirOrFile = $input->getOption('fixtures');
+        if ($dirOrFile) {
+            $paths = is_array($dirOrFile) ? $dirOrFile : array($dirOrFile);
+        } else {
+            $paths = $this->paths;
+        }
+
         $loader = new ServiceLocatorAwareLoader($this->serviceLocator);
-        $purger = new ORMPurger();
-
-        if ($input->getOption('purge-with-truncate')) {
-            $purger->setPurgeMode(self::PURGE_MODE_TRUNCATE);
+        foreach ($paths as $path) {
+            if (is_dir($path)) {
+                $loader->loadFromDirectory($path);
+            }
         }
-
-        $executor = new ORMExecutor($this->em, $purger);
-
-        foreach ($this->paths as $key => $value) {
-            $loader->loadFromDirectory($value);
+        $fixtures = $loader->getFixtures();
+        if (!$fixtures) {
+            throw new \InvalidArgumentException(
+                sprintf('Could not find any fixtures to load in: %s', "\n\n- ".implode("\n- ", $paths))
+            );
         }
-        $executor->execute($loader->getFixtures(), $input->getOption('append'));
+        $purger = new ORMPurger($em);
+        $purger->setPurgeMode($input->getOption('purge-with-truncate') ? ORMPurger::PURGE_MODE_TRUNCATE : ORMPurger::PURGE_MODE_DELETE);
+        $executor = new ORMExecutor($em, $purger);
+        $executor->setLogger(function($message) use ($output) {
+            $output->writeln(sprintf('  <comment>></comment> <info>%s</info>', $message));
+        });
+        $executor->execute($fixtures, $input->getOption('append'));
     }
 
     public function setPath($paths)
     {
-        $this->paths=$paths;
+        $this->paths = $paths;
     }
 
     public function setEntityManager($em)
